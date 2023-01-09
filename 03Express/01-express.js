130bdf601ff4f093fd51224ca4cc0e4029289c59
Luciaaf00 <luciaaf2000@gmail.com>
2023-01-09 19:09:14 +0100

    Express

---
+++ 03Express/01-express.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3005
+//Motor de plantillas 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs")
+
+
+//Middleware
+app.use(express.static(__dirname+ '/public'));
+
+
+app.get('/', (req, res) => {
+    //Primero indicamos la vista y después un objeto
+    res.render("index", {titulo: "mi titulo dinámico"})
+})
+app.get('/contacto', (req, res) => {
+    res.render("contacto", {tituloContacto: "Estamos en contacto de manera dinámica"})
+})
+app.use((req,res)=> {
+    //res.status(404).sendFile(__dirname+"/public/404.html")
+    res.status(404).render("404", {
+        titulo: "Error 404",
+        descripcion: "Page Not Found"
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
