a02ac9ba14c23674765d700c3559d10e2beb3efa
Luciaaf00 <luciaaf2000@gmail.com>
2023-01-11 18:58:52 +0100

    seguimos con express

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -10,13 +10,19 @@ app.set("view engine", "ejs")
 app.use(express.static(__dirname+ '/public'));
 
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     //Primero indicamos la vista y después un objeto
     res.render("index", {titulo: "mi titulo dinámico"})
 })
 app.get('/contacto', (req, res) => {
     res.render("contacto", {tituloContacto: "Estamos en contacto de manera dinámica"})
 })
+*/
+//Llamadas a las rutas
+app.use('/', require('./router/rutas'))
+app.use('/pokemon', require('./router/pokemon'))
+
+//Error controller
 app.use((req,res)=> {
     //res.status(404).sendFile(__dirname+"/public/404.html")
     res.status(404).render("404", {
